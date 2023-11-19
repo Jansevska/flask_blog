14a0f178bcc8378ba1c663b2dde04f03c06c7bf0
Emili <ejanchevis@gmail.com>
2023-11-19 13:42:15 -1000

    Update so we can use environment variables

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # C:\\Users\\bstan\\....\\week6\\flask
 
 class Config:
-    SECRET_KEY = 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     # C:\\Users\\bstan\\....\\week6\\flask\\app.db
