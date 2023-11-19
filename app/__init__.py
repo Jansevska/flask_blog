1893cf9ceb4da12e81b53aa51f6cd2eca14d670b
Emili <ejanchevis@gmail.com>
2023-11-19 12:13:45 -1000

    fetching

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,16 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_cors import CORS
 
 
 # Create an instance of Flask class
 app = Flask(__name__)
 # Configuring our app with Attributes and Values from the Config class --> CREATED AFTER CREATION OF FORMS!!!! <-- app.config['SECRET_KEY'] = 'you-will-never-guess > moved to config.py folder!
 app.config.from_object(Config)
+
+# Add CORS to our app
+CORS(app)
 
 # Create an instance of SQLAlchemy to represent our database
 db = SQLAlchemy(app) # the app as instance of Flask
